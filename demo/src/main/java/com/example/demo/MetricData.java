112a51db51973f5c5795e40070f912f6e24a0e61
Nguyen Anh Tu <nguyen.anh.tu.dev@gmail.com>
2024-08-22 10:54:27 +0700

    ok

---
+++ demo/src/main/java/com/example/demo/MetricData.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class MetricData {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private int metricValue;
+
+  private LocalDateTime updatedAt;
+}
--- demo/src/main/java/com/example/demo/ScheduledTasks.java
+++ demo/src/main/java/com/example/demo/ScheduledTasks.java
@@ -2,17 +2,38 @@ package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Slf4j
 @Component
-class ScheduledTasks {
-    @Scheduled(cron = "*/3 * * * * *") // Cấu hình để chạy mỗi 3 giây
-    @SchedulerLock(name = "TaskScheduler_scheduledTask",
-            lockAtLeastFor = "PT57S", // Khóa ít nhất 57 giây
-            lockAtMostFor = "PT1M") // Khóa tối đa 1 phút
-    public void scheduledTask() {
-        log.info("TaskScheduler.scheduledTask - Task executed");
+public class ScheduledTasks {
+
+    private final MetricDataRepository metricDataRepository;
+
+    @Autowired
+    public ScheduledTasks(MetricDataRepository metricDataRepository) {
+        this.metricDataRepository = metricDataRepository;
+    }
+
+    @Scheduled(cron = "*/10 * * * * *") // Chạy mỗi 10 giây
+    @SchedulerLock(name = "TaskScheduler_updateMetricData",
+          lockAtLeastFor = "PT20S",
+          lockAtMostFor = "PT30S")
+    public void updateMetricData() {
+        log.info("Attempting to update metric data...");
+
+        Optional<MetricData> dataOpt = metricDataRepository.findById(1L);
+        MetricData data = dataOpt.orElse(new MetricData());
+        data.setMetricValue(data.getMetricValue() + 1);
+        data.setUpdatedAt(LocalDateTime.now());
+
+        metricDataRepository.save(data);
+
+        log.info("MetricData updated to value: {}", data.getMetricValue());
     }
 }
